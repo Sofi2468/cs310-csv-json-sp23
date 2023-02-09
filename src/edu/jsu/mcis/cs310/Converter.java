50564736af7de669ee10cce0d51ae66fefa7a8d6
Sophia <sajemba@jsu.edu>
2023-02-08 21:16:02 -0600

    completed

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -1,5 +1,8 @@
 package edu.jsu.mcis.cs310;
 
+
+import java.io.*;
+import java.util.*;
 import com.github.cliftonlabs.json_simple.*;
 import com.opencsv.*;
 
@@ -74,12 +77,68 @@ public class Converter {
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
         
-        String result = "{}"; // default return value; replace later!
+        String result= "{}"; 
         
         try {
         
-            // INSERT YOUR CODE HERE
             
+            // Create a CSV reader from the CSV string
+            CSVReader reader = new CSVReader(new StringReader(csvString));
+            
+            // Read the header row of the CSV file
+            String[] header = reader.readNext();
+            
+            // Create a list to hold the data from the CSV file
+            List<String[]> rows = new ArrayList<>();
+            String[] row;
+            
+            // Read the data rows from the CSV file
+            while((row = reader.readNext()) != null) {
+                rows.add(row);
+            }
+            
+            // Create a JsonObject to hold the JSON data
+            JsonObject obj = new JsonObject();
+            
+            // Add the header row to the JsonObject as the "ColHeadings" key
+            obj.put("ColHeadings", header);
+            
+            // Create a list to hold the data from the CSV file
+            List<String> prodNums = new ArrayList<>();
+            List<List<Object>> data = new ArrayList<>();
+            
+            // Iterate over the data rows
+            for(String[] rowData : rows) {
+                // Add the "ProdNum" column to the "ProdNums" list
+                prodNums.add(rowData[0]);
+                
+                // Create a list to hold the data from the current row
+                List<Object> dataRow = new ArrayList<>();
+                
+                // Iterate over the columns in the current row
+                for(int i = 1; i < rowData.length; i++) {
+                    // Check if the current column is an integer
+                    try {
+                        dataRow.add(Integer.parseInt(rowData[i]));
+                    }
+                    catch(NumberFormatException e) {
+                        dataRow.add(rowData[i]);
+                    }
+                }
+                
+                // Add the data from the current row to the "Data" list
+                data.add(dataRow);
+            }
+            
+            // Add the "ProdNums" list to the JsonObject
+            obj.put("ProdNums", prodNums);
+            
+            // Add the "Data" list to the JsonObject
+            obj.put("Data", data);
+            
+            // Convert the JsonObject to a string
+            result = obj.toJson();
+ 
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -92,11 +151,43 @@ public class Converter {
     @SuppressWarnings("unchecked")
     public static String jsonToCsv(String jsonString) {
         
-        String result = ""; // default return value; replace later!
+        String result= "{}"; 
         
         try {
             
-            // INSERT YOUR CODE HERE
+        // Create a JsonObject from the JSON string
+        JsonObject json = (JsonObject) Jsoner.deserialize(jsonString);
+        
+        // Get the header row from the JsonObject, using the "ColHeadings" key
+        List<String> header = (List<String>) json.get("ColHeadings");
+        
+        // Get the "Data" key from the JsonObject
+        List<List<Object>> data = (List<List<Object>>) json.get("Data");
+        
+        // Get the "ProdNums" key from the JsonObject
+        List<String> prodNums = (List<String>) json.get("ProdNums");
+        
+        // Create a CSV writer
+        StringWriter stringWriter = new StringWriter();
+        CSVWriter writer = new CSVWriter(stringWriter);
+        
+        // Write the header row to the CSV writer
+        writer.writeNext(header.toArray(new String[0]));
+        
+        // Write the data rows to the CSV writer
+        for (int i = 0; i < data.size(); i++) {
+            List<Object> rowData = data.get(i);
+            String[] rowWithProdNum = new String[rowData.size() + 1];
+            rowWithProdNum[0] = prodNums.get(i);
+            for (int j = 0; j < rowData.size(); j++) {
+                rowWithProdNum[j + 1] = rowData.get(j).toString();
+            }
+            writer.writeNext(rowWithProdNum);
+        }
+        writer.close();
+        
+        // Get the string representation of the CSV data
+        result = stringWriter.toString();
             
         }
         catch (Exception e) {
